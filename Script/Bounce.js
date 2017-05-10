5951b9b24c81632460092ea74509942cc73d9650
Ethan Barker <ethanbarker0@gmail.com>
2017-05-09 22:16:03 -0700

    Init

---
+++ Script/Bounce.js
@@ -0,0 +1,85 @@
+function setup() {
+    createCanvas(600, 400);
+}
+
+let xv = 0;
+let yv = 0;
+let x = 0;
+let y = 0;
+let velocity = 1;
+let maxv = 3;
+let friction = 0.3;
+
+let players;
+
+function physics() {
+    if (xv > 0) {
+        xv -= friction;
+        if (xv < 0) {
+            xv = 0;
+        }
+    } else {
+        xv += friction;
+        if (xv > 0) {
+            xv = 0;
+        }
+    }
+    if (yv > 0) {
+        yv -= friction;
+        if (yv < 0) {
+            yv = 0;
+        }
+    } else {
+        yv += friction;
+        if (yv > 0) {
+            yv = 0;
+        }
+    }
+
+    if (xv > maxv) {
+        xv = maxv;
+    } else if (xv < -maxv) {
+        xv = -maxv
+    }
+    if (yv > maxv) {
+        yv = maxv;
+    } else if (yv < -maxv) {
+        yv = -maxv
+    }
+
+    x += xv;
+    y += yv;
+}
+
+function draw() {
+    background(0);
+    if (keyIsDown(LEFT_ARROW))
+        xv -= velocity;
+
+    if (keyIsDown(RIGHT_ARROW))
+        xv += velocity;
+
+    if (keyIsDown(UP_ARROW))
+        yv -= velocity;
+
+    if (keyIsDown(DOWN_ARROW))
+        yv += velocity;
+    physics();
+    fill(255);
+    ellipse(x, y, 20, 20);
+
+    if (logedin) {
+        database.ref('Users/' + uid).set({
+            display: username,
+            x: x,
+            y: y
+        });
+        if (players) {
+            for (let key in players) {
+                if (!players.hasOwnProperty(key)) continue;
+                let player = players[key];
+                ellipse(player["x"], player["y"], 20, 20);
+            }
+        }
+    }
+}
---
+++ Script/firebase.js
@@ -0,0 +1,83 @@
+var config = {
+    apiKey: "AIzaSyAD6rxrGfFQmImBrP2JnxCRE3l5k8SjpNA",
+    authDomain: "bounce-72a28.firebaseapp.com",
+    databaseURL: "https://bounce-72a28.firebaseio.com",
+    projectId: "bounce-72a28",
+    storageBucket: "bounce-72a28.appspot.com",
+    messagingSenderId: "69347143131"
+};
+firebase.initializeApp(config);
+let auth, database, storage, user, token;
+let username, email, photoUrl, uid, emailVerified;
+let logedin = false;
+
+$(document).ready(function () {
+    init();
+    $(".login").click(function () {
+        if (user) {
+            logout();
+        } else {
+            login();
+        }
+    });
+    $(".logout").click(function () {
+        logout();
+    });
+});
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        updateLogin();
+        $(".username").text(username);
+        $(".userpic").attr("src", photoUrl);
+        $(".login").text("Sign out");
+        let commentsRef = firebase.database().ref('Users');
+        commentsRef.on('value', function(data) {
+            players = data.val();
+        });
+        logedin = true;
+    } else {
+        $(".login").text("Sign in");
+        $(".username").text("Not Logged In");
+        $(".userpic").attr("src", "https://developers.google.com/experts/img/user/user-default.png");
+        logedin = false;
+    }
+});
+
+function login() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        token = result.credential.accessToken;
+        // The signed-in user info.
+    }).catch(function(error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        let email = error.email;
+        console.log("Uh oh, something bad happened: ");
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+}
+
+function updateLogin() {
+    user = firebase.auth().currentUser;
+    username = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;
+}
+
+function init() {
+    auth = firebase.auth();
+    database = firebase.database();
+    storage = firebase.storage();
+    // auth.onAuthStateChanged(authChange());
+}
+function logout() {
+    firebase.auth().signOut().then(function() {
+        user = null;
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });
+}
